594f7e93759d35a94d6b2351e859b14330721d17
fellypr <fellype29kennedemil@gmail.com>
2025-07-20 22:46:58 -0300

    Ultima Do dia

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Simulando o __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   server: {
     historyApiFallback: true, 
   },
